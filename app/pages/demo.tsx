0d99178bd26a2ee9950fe3382afa232c0798f351
tyshgc <hello@tyshgc.pw>
2021-02-16 20:44:36 +0900

    wip

--- app/pages/demo.tsx
+++ app/pages/demo.tsx
@@ -2,7 +2,7 @@ import { BlitzPage } from "blitz"
 import Layout from "app/layouts/Layout"
 const db = process.env.DATABASE_URL
 const Demo: BlitzPage = () => {
-  return <div>demo, ${db}</div>
+  return <div>demo, {db}</div>
 }
 
 Demo.getLayout = (page) => <Layout title="Home">{page}</Layout>
